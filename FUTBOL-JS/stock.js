cf4d3bc6bf03c5bf2cf1d816b51098aaf10550ea
zarpadoen <yaircarbia420@gmail.com>
2022-04-25 20:50:05 -0300

    Entrega

---
+++ FUTBOL-JS/stock.js
@@ -0,0 +1,54 @@
+
+let stockProductos = [
+    { id: 1, nombre: "Pelota Penalty", tipo: "pelota", precio: 2202, talle: "3", img: "./img/ppenalty.jpg" },
+    { id: 2, nombre: "Pelota Penalty", tipo: "pelota", precio: 2202, talle: "4", img: "./img/ppenalty.jpg" },
+    { id: 3, nombre: "Pelota Penalty", tipo: "pelota", precio: 2202, talle: "5", img: "./img/ppenalty.jpg" },
+    { id: 4, nombre: "Pelota Penalty", tipo: "pelota", precio: 2202, talle: "5", img: "./img/ppenalty.jpg" },
+    { id: 5, nombre: "Pelota Penalty", tipo: "pelota", precio: 2202, talle: "5", img: "./img/ppenalty.jpg" },
+
+    { id: 6, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "3", img: "./img/ptoper.jpg" },
+    { id: 7, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "3", img: "./img/ptoper.jpg" },
+    { id: 8, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "3", img: "./img/ptoper.jpg" },
+    { id: 9, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "4", img: "./img/ptoper.jpg" },
+    { id: 10, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "5", img: "./img/ptoper.jpg" },
+    { id: 11, nombre: "Pelota Topper", tipo: "pelota", precio: 1990, talle: "5", img: "./img/ptoper.jpg" },
+
+    { id: 12, nombre: "Pelota Nike", tipo: "pelota", precio: 3000, talle: "4", img: "./img/pnike.jpg" },
+    { id: 13, nombre: "Pelota Nike", tipo: "pelota", precio: 3000, talle: "4", img: "./img/pnike.jpg" },
+    { id: 14, nombre: "Pelota Nike", tipo: "pelota", precio: 3000, talle: "5", img: "./img/pnike.jpg" },
+    { id: 15, nombre: "Pelota Nike", tipo: "pelota", precio: 3000, talle: "5", img: "./img/pnike.jpg" },
+    { id: 16, nombre: "Pelota Nike", tipo: "pelota", precio: 3000, talle: "5", img: "./img/pnike.jpg" },
+
+    { id: 17, nombre: "Pelota Puma", tipo: "pelota", precio: 2400, talle: "3", img: "./img/ppuma.jpg" },
+    { id: 18, nombre: "Pelota Puma", tipo: "pelota", precio: 2400, talle: "3", img: "./img/ppuma.jpg" },
+    { id: 19, nombre: "Pelota Puma", tipo: "pelota", precio: 2400, talle: "3", img: "./img/ppuma.jpg" },
+    { id: 20, nombre: "Pelota Puma", tipo: "pelota", precio: 2400, talle: "4", img: "./img/ppuma.jpg" },
+    { id: 21, nombre: "Pelota Puma", tipo: "pelota", precio: 2400, talle: "5", img: "./img/ppuma.jpg" },
+
+    { id: 22, nombre: "Botines Umbro", tipo: "botin", precio: 2220, talle: "S", img: "./img/bumbro.jpg" },
+    { id: 23, nombre: "Botines Umbro", tipo: "botin", precio: 2220, talle: "M", img: "./img/bumbro.jpg" },
+    { id: 24, nombre: "Botines Umbro", tipo: "botin", precio: 2220, talle: "L", img: "./img/bumbro.jpg" },
+    { id: 25, nombre: "Botines Umbro", tipo: "botin", precio: 2220, talle: "XL", img: "./img/bumbro.jpg" },
+
+    { id: 26, nombre: "Botines Nike", tipo: "botin", precio: 2220, talle: "S", img: "./img/bnike.jpg" },
+    { id: 27, nombre: "Botines Nike", tipo: "botin", precio: 2220, talle: "M", img: "./img/bnike.jpg" },
+    { id: 28, nombre: "Botines Nike", tipo: "botin", precio: 2220, talle: "L", img: "./img/bnike.jpg" },
+    { id: 29, nombre: "Botines Nike", tipo: "botin", precio: 2220, talle: "XL", img: "./img/bnike.jpg" },
+
+    { id: 30, nombre: "Botines Adidas", tipo: "botin", precio: 2220, talle: "S", img: "./img/badiadas.jpg" },
+    { id: 31, nombre: "Botines Adidas", tipo: "botin", precio: 2220, talle: "M", img: "./img/badiadas.jpg" },
+    { id: 32, nombre: "Botines Adidas", tipo: "botin", precio: 2220, talle: "L", img: "./img/badiadas.jpg" },
+    { id: 33, nombre: "Botines Adidas", tipo: "botin", precio: 2220, talle: "XL", img: "./img/badiadas.jpg" },
+
+    { id: 34, nombre: "Botines Puma", tipo: "botin", precio: 2220, talle: "S", img: "./img/bpuma.jpg" },
+    { id: 35, nombre: "Botines Puma", tipo: "botin", precio: 2220, talle: "M", img: "./img/bpuma.jpg" },
+    { id: 36, nombre: "Botines Puma", tipo: "botin", precio: 2220, talle: "L", img: "./img/bpuma.jpg" },
+    { id: 37, nombre: "Botines Puma", tipo: "botin", precio: 2220, talle: "XL", img: "./img/bpuma.jpg" },
+
+    { id: 38, nombre: "Pelota Adidas", tipo: "pelota", precio: 2220, img: "./img/padidas.jpg" },
+    { id: 39, nombre: "Pelota Adidas", tipo: "pelota", precio: 2220, img: "./img/padidas.jpg" },
+    { id: 40, nombre: "Pelota Adidas", tipo: "pelota", precio: 2220, img: "./img/padidas.jpg" },
+    { id: 41, nombre: "Pelota Adidas", tipo: "pelota", precio: 2220, img: "./img/padidas.jpg" }
+
+
+];
